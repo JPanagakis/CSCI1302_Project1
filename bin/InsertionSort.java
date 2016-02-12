c4ce685245ab73c203e21ecd768538fe5ded6141
JPanagakis <justinpanagakis@yahoo.com>
2016-02-12 02:53:11 -0500

    Completed Algorithm

--- bin/InsertionSort.java
+++ bin/InsertionSort.java
@@ -2,4 +2,42 @@
  * Created by justin on 2/12/16.
  */
 public class InsertionSort {
+
+    private long time;
+    private StopWatch stopWatch;
+    private int[] array;
+
+    public InsertionSort(int[] a){
+
+        stopWatch = new StopWatch();
+        array = a;
+
+        doSort(array);
+    }
+
+    public void doSort(int[] array){
+
+        stopWatch.start();
+
+        int x;
+        int j;
+
+        for (int i = 1; i < array.length; i++){
+            x = array[i];
+            j = i - 1;
+            while (j >= 0 && array[j] > x){
+
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = x;
+        }
+
+        stopWatch.stop();
+        time = stopWatch.getElapsedTime();
+    }
+
+    public long getTime(){return time;}
+
+    public int[] getSortedArray(){return array;}
 }
--- bin/InterpolationSearch.java
+++ bin/InterpolationSearch.java
@@ -2,4 +2,54 @@
  * Created by justin on 2/12/16.
  */
 public class InterpolationSearch {
+
+    private int target;
+    private long time;
+    private StopWatch stopWatch;
+    private int[] array;
+
+    public InterpolationSearch(int[] a, int size, int key){
+
+        stopWatch = new StopWatch();
+        array = a;
+
+        target = doSearch(array, size, key);
+    }
+
+    public int doSearch(int[] array, int size, int key){
+
+        stopWatch.start();
+
+        int low = 0;
+        int high = size - 1;
+        int mid;
+
+        while (array[high] != array[low] && key >= array[low] && key <= array[high]){
+            mid = low + ((key - array[low]) * (high - low) / (array[high] - array[low]));
+
+            if (array[mid] < key){
+                low = mid + 1;
+            } else if (key < array[mid]){
+                high = mid - 1;
+            } else {
+                stopWatch.stop();
+                time = stopWatch.getElapsedTime();
+                return mid;
+            }
+        }
+
+        if (key == array[low]){
+            stopWatch.stop();
+            time = stopWatch.getElapsedTime();
+            return low;
+        } else {
+            stopWatch.stop();
+            time = stopWatch.getElapsedTime();
+            return -1;
+        }
+    }
+
+    public long getTime(){return time;}
+
+    public int getTarget(){return target;}
 }
