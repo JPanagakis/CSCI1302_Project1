a56c368aefe2a43efd53f410bcd3d90a2373cceb
JPanagakis <justinpanagakis@yahoo.com>
2016-02-11 23:43:13 -0500

    Made AlgorithmTester.java functional

--- bin/AlgorithmTester.java
+++ bin/AlgorithmTester.java
@@ -2,4 +2,17 @@
  * Created by justin on 2/11/16.
  */
 public class AlgorithmTester {
+
+    public static void main(String[] args){
+
+        private int TEST_ARRAY_SIZE = 20000;
+        ArrayUtil arrayUtil = new ArrayUtil();
+
+        int[] testArray = arrayUtil.randomIntArray(TEST_ARRAY_SIZE, 100);
+
+        sortSearchAlgorithm1 s1 = new sortSearchAlgorithm(testArray);
+
+        System.out.println(s1.getTime() + "ms");
+        System.out.println("Congrats, this works");
+    }
 }
