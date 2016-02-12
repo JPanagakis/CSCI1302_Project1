1ae9a220a85c288d4c5eb107f4a3af6ae0e8b51e
JPanagakis <justinpanagakis@yahoo.com>
2016-02-12 02:51:12 -0500

    Changing window size

--- bin/AlgorithmEfficiency.java
+++ bin/AlgorithmEfficiency.java
@@ -3,12 +3,15 @@ import javax.swing.*;
 /**
  * Created by justin on 2/8/16.
  */
+
+//We cannot use Binary Search, Linear Search, Selection Sort, or Merge Sort
+
 public class AlgorithmEfficiency extends JFrame {
 
     public AlgorithmEfficiency(){
 
         setTitle("Algorithm Efficiency");
-        setSize(800, 550);
+        setSize(1200, 550);
         setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
