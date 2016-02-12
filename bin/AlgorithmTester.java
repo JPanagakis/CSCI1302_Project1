d55e531acf605f683fcb649c147a7a6527ad53cb
JPanagakis <justinpanagakis@yahoo.com>
2016-02-12 02:57:25 -0500

    Update for accuracy, added Search compatibility

--- bin/AlgorithmTester.java
+++ bin/AlgorithmTester.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Created by justin on 2/11/16.
  */
@@ -5,14 +7,53 @@ public class AlgorithmTester {
 
     public static void main(String[] args){
 
-        private int TEST_ARRAY_SIZE = 20000;
+        Random random = new Random();
+
+        int TEST_ARRAY_SIZE = 10;
+        boolean sortWorks = true;
         ArrayUtil arrayUtil = new ArrayUtil();
 
         int[] testArray = arrayUtil.randomIntArray(TEST_ARRAY_SIZE, 100);
 
-        sortSearchAlgorithm1 s1 = new sortSearchAlgorithm(testArray);
+        int key = random.nextInt(TEST_ARRAY_SIZE);
 
-        System.out.println(s1.getTime() + "ms");
-        System.out.println("Congrats, this works");
+        //put your Sort algorithm Class name here and un-blockComment
+
+        //InsertionSort s1 = new InsertionSort(testArray);
+
+        /*
+        int[] sortedArray = s1.getSortedArray();
+
+        System.out.println("\nSort");
+        System.out.println("----------------------------------");
+        System.out.println("Time: " + s1.getTime() + "ms");
+        for (int i = 0; i < sortedArray.length; i++){
+            System.out.print("[" + sortedArray[i] + "] ");
+        }
+        for (int i = 0; i < sortedArray.length - 1; i++){
+
+            if (i > i + 1){
+                sortWorks = false;
+            }
+        }
+        System.out.println("\nWorks: " + sortWorks);
+        */
+
+        //use this section for Search algorithms
+
+        //InterpolationSearch s5 = new InterpolationSearch(sortedArray, sortedArray.length, testArray[key]);
+
+        /*
+        System.out.println("\nSearch");
+        System.out.println("----------------------------------");
+        System.out.println("Time: " + s5.getTime() + "ms");
+        System.out.println("Target: " + testArray[key]);
+        System.out.println("Target Index: " + s5.getTarget());
+        if (sortedArray[s5.getTarget()] == testArray[key]){
+            System.out.println("Works: true");
+        } else {
+            System.out.println("Works: false");
+        }
+        */
     }
 }
