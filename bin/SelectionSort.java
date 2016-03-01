6f684110dc3b964546f39f6cd97eecca089c41a3
JPanagakis <justinpanagakis@yahoo.com>
2016-03-01 18:23:39 -0500

    Adding finishing touches

--- bin/SelectionSort.java
+++ bin/SelectionSort.java
@@ -1,27 +1,41 @@
+import javax.swing.*;
+
 public class SelectionSort {
- 
- public SelectionSort(int[] a) {
-    int[] array = a;
-    int[] sortedArray = doSelectionSort(array);
- }
- 
-    public static int[] doSelectionSort(int[] arr){
-         
+
+    private long time;
+    private int[] sortedArray;
+    private StopWatch stopWatch;
+
+    public SelectionSort(int[] a) {
+
+        stopWatch = new StopWatch();
+        doSelectionSort(a);
+    }
+
+    public void doSelectionSort(int[] arr){
+
+        stopWatch.start();
+
         for (int i = 0; i < arr.length - 1; i++)
         {
             int index = i;
             for (int j = i + 1; j < arr.length; j++)
                 if (arr[j] < arr[index])
                     index = j;
-      
-            int smallerNumber = arr[index]; 
+
+            int smallerNumber = arr[index];
             arr[index] = arr[i];
             arr[i] = smallerNumber;
         }
-        return arr;
+        sortedArray = arr;
+        stopWatch.stop();
+        time = stopWatch.getElapsedTime();
     }
     public int[] getSortedArray() {
         return sortedArray;
-    } 
-    
     }
+
+    public long getTime(){
+        return time;
+    }
+}
